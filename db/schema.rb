ca0f4df1684ecb2bac4d169f1f9eeda4e1bf683e
n.isobe <ganmodoki7.43dari@gmail.com>
2020-09-23 20:24:20 +0900

    productモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_045049) do
+ActiveRecord::Schema.define(version: 2020_09_23_080150) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.string "unit", default: "yen", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
